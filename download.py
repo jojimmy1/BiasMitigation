f828e8402a8db1c0f01e2e39a3bbeabfb864b04f
ZC Li <li3975@purdue.edu>
2024-10-24 20:26:58 -0400

    clone codes for pre and in

---
+++ download.py
@@ -0,0 +1,18 @@
+# import kagglehub
+
+# # Download latest version
+# path = kagglehub.dataset_download("lainguyn123/student-performance-factors")
+
+# print("Path to dataset files:", path)
+# import kagglehub
+
+# # Download latest version
+# path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
+
+# print("Path to dataset files:", path)
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("kiva/data-science-for-good-kiva-crowdfunding")
+
+print("Path to dataset files:", path)
