3839a2276f4767ce7e7d5d989d0d4efe6ea56ce9
jojimmy <jojimmy90@gmail.com>
2024-11-21 17:09:22 -0500

    Correlations higher than 0.1 and lower than 1: Hours_Studied and Exam_Score: 0.44545495407528174 Attendance and Exam_Score: 0.5810718633120632 Previous_Scores and Exam_Score: 0.17507908702291072 Tutoring_Sessions and Exam_Score: 0.1565251853922526 Exam_Score and Peer_Influence_encoded: 0.10021651642189966

--- pre-pro-draft/coeff.py
+++ pre-pro-draft/coeff.py
@@ -10,6 +10,10 @@ le = LabelEncoder()
 df['gender_encoded'] = le.fit_transform(df['Gender'])
 df['family_income_encoded'] = le.fit_transform(df['Family_Income'])
 
+# Encode all categorical variables
+for column in df.select_dtypes(include=['object']).columns:
+    df[f'{column}_encoded'] = le.fit_transform(df[column])
+
 # Select numerical columns
 numerical_cols = df.select_dtypes(include=['int64', 'float64']).columns
 
@@ -21,9 +25,21 @@ gender_corr = correlations['gender_encoded'].drop('gender_encoded')
 family_income_corr = correlations['family_income_encoded'].drop('family_income_encoded')
 
 print(correlations)
+# save to csv
+correlations.to_csv('data/correlations.csv')
+# find corr higher than 0.1
+print("\nCorrelations higher than 0.1 and lower than 1:")
+# print(correlations[(correlations > 0.1) & (correlations < 1)])
+for i in range(len(correlations)):
+    for j in range(i+1, len(correlations)):
+        if correlations.iloc[i, j] > 0.1 and correlations.iloc[i, j] < 1:
+            print(f"{correlations.columns[i]} and {correlations.columns[j]}: {correlations.iloc[i, j]}")
 
 print("\nCorrelation with Gender:")
 print(gender_corr)
 print("\nCorrelation with Family Income:")
 print(family_income_corr)
+
+# correlations = df.corr()
+# print(correlations)
 
